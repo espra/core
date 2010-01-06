8a211dcc13895dfc52dba6bf6b6827ef439bec84
tav <tav@espians.com>
2010-01-06 09:14:43 +0000

    The siteinfo file for tentapp.com.

---
+++ appengine/siteinfo.py
@@ -0,0 +1,6 @@
+# Released into the Public Domain by tav <tav@espians.com>
+
+"""Deployment site specific info -- for use by tentapp.core.config."""
+
+SITE_DOMAIN = 'tentapp.com'
+SITE_HTTP_URL = 'http://www.tentapp.com'
