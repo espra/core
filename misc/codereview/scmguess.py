ff4fb4b652a487d48be9fe05a88e05c7313d123d
tav <tav@espians.com>
2010-01-20 19:47:57 +0000

    Added a module to guess the SCM being used.

---
+++ misc/codereview/scmguess.py
@@ -0,0 +1,64 @@
+# Authored by the Ampify Contributors. Use of this source code is governed by a
+# Public Domain style license that can be found in the LICENSE file.
+
+"""SCM detection utility functions."""
+
+import os
+import upload
+
+
+def IsGit():
+    """Return whether the current directory is inside a Git repo."""
+
+    _, error = upload.RunShellWithReturnCode(
+        ["git", "rev-parse", "--is-inside-work-tree"]
+        )
+    if not error:
+        return True
+
+
+def IsMercurial():
+    """Return whether the current directory is inside a Mercurial repo."""
+
+    _, error = upload.RunShellWithReturnCode(["hg", "root"])
+    if not error:
+        return True
+
+
+def IsSubversion():
+    """Return whether the current directory is inside a Subversion repo."""
+
+    if os.path.isdir('.svn'):
+        return True
+
+
+def GuessSCM(priority='git'):
+    """Tries to guess the SCM being used in the current directory."""
+
+    if priority == 'git':
+        if IsGit():
+            return 'git'
+        if IsMercurial():
+            return 'hg'
+        if IsSubversion():
+            return 'svn'
+    elif priority == 'hg':
+        if IsMercurial():
+            return 'hg'
+        if IsGit():
+            return 'git'
+        if IsSubversion():
+            return 'svn'
+    elif priority == 'svn':
+        if IsSubversion():
+            return 'svn'
+        if IsGit():
+            return 'git'
+        if IsMercurial():
+            return 'hg'
+    else:
+        raise ValueError("Unknown SCM passed as the priority: %r" % priority)
+
+
+if __name__ == '__main__':
+    print GuessSCM()
