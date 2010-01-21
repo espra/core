6e66ae0c4a4ee1522323a8a96ae6d3d72aaafb99
evangineer <mamading@gmail.com>
2010-01-21 01:21:06 +0000

    Small cleanup and addition of a bit of test code for Paypal Adaptive Payments API implementation

---
+++ client_libs/appengine/sample_app/ampify/util/test_paypalx.py
@@ -0,0 +1,16 @@
+from ampify.util.paypalx import *
+from ampify.core.config import PPX_API_USERNAME, PPX_API_PASSWORD, \
+    PPX_API_SIGNATURE, PPX_APP_ID
+
+api_credentials = {'api_username': PPX_API_USERNAME, \
+                    'api_password': PPX_API_PASSWORD, \
+                    'api_signature': PPX_API_SIGNATURE, \
+                    'app_id': PPX_APP_ID}
+adaptpay = AdaptivePayment(**api_credentials)
+pay_request_kwargs = { \
+            'receiver_email': 'mamadi_1262634965_per@gmail.com', \
+            'receiver_amount': 20.00, 'currency_code': 'GBP', \
+            'cancel_url': 'http://www.ebay.com', \
+            'return_url': 'http://www.sandbox.paypal.com'}
+payreq = PayRequest(**pay_request_kwargs)
+payreqmsg = payreq.get_message()
