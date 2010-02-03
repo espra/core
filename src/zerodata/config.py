6d2e3141e4472e696182853733114dc1002cc13a
tav <tav@espians.com>
2010-02-03 04:37:02 +0000

    Some support files for the zerodata app.

--- src/zerodata/config.py
+++ src/zerodata/config.py
@@ -1,2 +1,23 @@
 # No Copyright (-) 2010 The Ampify Authors. This file is under the
 # Public Domain license that can be found in the root LICENSE file.
+
+"""App configuration for zerodata."""
+
+__all__ = [
+    'DEBUG'
+    ]
+
+# ------------------------------------------------------------------------------
+# the settings
+# ------------------------------------------------------------------------------
+
+DEBUG = True
+
+# ------------------------------------------------------------------------------
+# override sample config values with the real "secret" ones
+# ------------------------------------------------------------------------------
+
+try:
+    from secret import *
+except:
+    pass
