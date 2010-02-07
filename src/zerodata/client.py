eaa4b914725561690e73ca7825f0cf8b00700892
evangineer <mamading@gmail.com>
2010-02-07 15:11:11 +0000

    Added a call function

--- src/zerodata/client.py
+++ src/zerodata/client.py
@@ -19,18 +19,29 @@ sys.path.insert(0, join_path(AMPIFY_ROOT, 'environ', 'startup'))
 
 import rconsole
 
+# ------------------------------------------------------------------------------
+# some konstants
+# ------------------------------------------------------------------------------
+API_OPERATIONS = ['get', 'delete', 'invalidate', 'put', 'query']
+PRODUCTION_ENDPOINT = "http://ampify.appspot.com"
+TEST_ENDPOINT = "http://localhost"
+
 
 class ZeroDataClient(object)
     """Provides a client for ZeroDataStore"""
 
     def __init__(self):
+        self._endpoint = TEST_ENDPOINT
         # Create an OpenerDirector with support for SSL and other stuff...
         opener = self.build_opener(True) 
         # ...and install it globally so it can be used with urlopen. 
         install_opener(opener) 
-        urlopen(’https://github.com’)
+        try:
+            openurl = urlopen(self._endpoint)
+        except URLError:
+            raise RuntimeError("API endpoint not available")
 
-    def build_opener(debug=False):
+    def build_opener(self, debug=False):
         """Create handlers with the appropriate debug level.  
         We intentionally create new ones because the OpenerDirector 
         class in urllib2 is smart enough to replace its internal 
@@ -38,7 +49,7 @@ class ZeroDataClient(object)
         urllib2.build_opener method.  This is much easier than 
         trying to introspect into the OpenerDirector to find the 
         existing handlers.
-        based on http://code.activestate.com/recipes/440574/#c1
+        Based on http://code.activestate.com/recipes/440574/#c1
         """
         http_handler = HTTPHandler(debuglevel=debug)
         https_handler = HTTPSHandler(debuglevel=debug)
@@ -52,4 +63,18 @@ class ZeroDataClient(object)
         opener = build_opener(handlers)
 
         return opener
+
+    def call(self, api_operation, api_request):
+        try:
+            API_OPERATIONS.index(api_operation)
+        except ValueError:
+            raise RuntimeError("Invalid API operation.")
+        url = "%s/%s" % (self._endpoint, api_operation)
+        req = http_request(url)
+        json_request = json_encode(api_request)
+        try:
+            openurl = urlopen(req, json_request)
+        except URLError:
+            raise RuntimeError("API request failed.")
+        return json_decode(openurl.read())
 
