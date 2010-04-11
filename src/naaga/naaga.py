9f8c92d7ae563f9879559603e7fc5e215ee56cf3
tav <tav@espians.com>
2010-04-11 15:23:24 +0100

    Adding a stub naaga.py with sbp's tokeniser.

---
+++ src/naaga/naaga.py
@@ -0,0 +1,32 @@
+#! /usr/bin/env python
+
+from unicodedata import category as unicategory
+from itertools import groupby
+from time import time
+
+def tokenise(text): 
+    groups = groupby(text, unicategory)
+    for category, token in groups: 
+        yield ''.join(token)
+
+def tokenise2(text): 
+    groups = groupby(text, unicategory)
+    return [''.join(token) for category, token in groups]
+
+text = open('src/foo.js', 'rb').read()
+text = unicode(text, 'utf-8')
+
+def bench(duration, func, *args):
+    print "benching <" + func.__name__ + "> for " + str(duration) + "s:"
+    total = 0
+    i = 0
+    while 1:
+        start = time()
+        func(*args)
+        total += time() - start
+        i += 1
+        if total > duration:
+            break
+    print total, '\t', i, 'runs'
+
+bench(2.0, tokenise2, text)
