6c71c15f181713810766e705ce33c0dee0ee60b5
tav <tav@espians.com>
2010-04-13 01:20:08 +0100

    Added a public domain declaration to makeucd.py.

--- src/naaga/makeucd.py
+++ src/naaga/makeucd.py
@@ -1,5 +1,8 @@
 #! /usr/bin/env python
 
+# No Copyright (-) 2010 The Ampify Authors. This file is under the
+# Public Domain license that can be found in the root LICENSE file.
+
 """
 ========================
 Javascript UCD Generator
