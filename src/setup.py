cdf025e76305e4d4ddee11216b6b2d253dc85ed0
tav <tav@espians.com>
2010-05-16 04:42:57 +0100

    Added a setup.py for compiling pyutil.pylzf.

---
+++ src/setup.py
@@ -0,0 +1,34 @@
+#! /usr/bin/env python
+
+# No Copyright (-) 2010 The Ampify Authors. This file is under the
+# Public Domain license that can be found in the root LICENSE file.
+
+import sys
+
+from distutils.core import Extension, setup
+
+# ------------------------------------------------------------------------------
+# the extensions
+# ------------------------------------------------------------------------------
+
+extensions = [
+    Extension(
+        "pyutil.pylzf",
+        ["pyutil/pylzf.c", "pyutil/lzf/lzf_c.c", "pyutil/lzf/lzf_d.c"],
+        include_dirs=["pyutil/lzf"],
+        )
+    ]
+
+# ------------------------------------------------------------------------------
+# run setup
+# ------------------------------------------------------------------------------
+
+if not sys.argv[1:]:
+    sys.argv.extend(['build_ext', '-i'])
+
+setup(
+    name="pyutil",
+    version="git",
+    description="A Python utility library",
+    ext_modules=extensions,
+    )
