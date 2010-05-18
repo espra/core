484337b06a45394eff9bbaffc3682b79622c76f6
tav <tav@espians.com>
2010-05-18 21:23:47 +0100

    Added license header to some pyutil files.

--- src/pyutil/compression.py
+++ src/pyutil/compression.py
@@ -1,3 +1,6 @@
+# No Copyright (-) 2010 The Ampify Authors. This file is under the
+# Public Domain license that can be found in the root LICENSE file.
+
 from pyutil.pylzf import compress, decompress
 
 HEADERS = """
