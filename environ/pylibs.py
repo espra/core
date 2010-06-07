5b998278ec6d3a11960cafca0d8b7e827dfbbccd
tav <tav@espians.com>
2010-06-07 15:32:36 +0100

    Updated environ scripts + new ampzero script.

--- environ/pylibs.py
+++ environ/pylibs.py
@@ -8,9 +8,13 @@ from os.path import dirname, join as join_path, realpath
 AMPIFY_ROOT = dirname(dirname(realpath(__file__)))
 PYUTIL_PATH = join_path(AMPIFY_ROOT, 'src')
 THIRD_PARTY_LIBS_PATH = join_path(AMPIFY_ROOT, 'third_party', 'pylibs')
+ZERO_PATH = join_path(AMPIFY_ROOT, 'src', 'zero')
 
 if PYUTIL_PATH not in sys.path:
     sys.path.insert(0, PYUTIL_PATH)
 
 if THIRD_PARTY_LIBS_PATH not in sys.path:
     sys.path.insert(0, THIRD_PARTY_LIBS_PATH)
+
+if ZERO_PATH not in sys.path:
+    sys.path.insert(0, ZERO_PATH)
