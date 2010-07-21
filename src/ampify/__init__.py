afbeab2e5af85ad575b8543bd9600c7bff5475c6
tav <tav@espians.com>
2010-07-21 06:44:02 +0100

    Added a Python package for Ampify Zero.

---
+++ src/ampify/__init__.py
@@ -0,0 +1,31 @@
+# No Copyright (-) 2010 The Ampify Authors. This file is under the
+# Public Domain license that can be found in the root LICENSE file.
+
+"""
+===================
+Ampify Zero Package
+===================
+
+This package implements version ``Zero`` of Ampify, a decentralised social
+platform.
+
+::
+
+                                   ___       
+                              _  /'___)      
+       _ _   ___ ___   _ _   (_)| (__  _   _ 
+     /'_` )/' _ ` _ `\( '_`\ | || ,__)( ) ( )
+    ( (_| || ( ) ( ) || (_) )| || |   | (_) |
+    `\__,_)(_) (_) (_)| ,__/'(_)(_)   `\__, |
+                      | |             ( )_| |
+                      (_)             `\___/'
+
+
+"""
+
+__version__ = (0, 0, 0)
+__release__ = '.'.join(map(str, __version__))
+
+__authors__ = {
+    0x01: ('tav', 'tav@espians.com')
+    }
