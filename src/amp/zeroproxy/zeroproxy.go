b07a227ff7df3b176edbf29f14dfd1ef1e4786bb
tav <tav@espians.com>
2010-09-05 05:24:37 +0100

    Implemented HTTP redirector for zeroproxy.

--- src/amp/zeroproxy/zeroproxy.go
+++ src/amp/zeroproxy/zeroproxy.go
@@ -28,13 +28,22 @@ import (
 )
 
 const (
-	ContentType   = "Content-Type"
-	ContentLength = "Content-Length"
-	TextHTML      = "text/html"
+	contentType      = "Content-Type"
+	contentLength    = "Content-Length"
+	redirectHTML     = `Please <a href="%s">click here if your browser doesn't redirect</a> automatically.`
+	redirectURL      = "%s%s"
+	redirectURLQuery = "%s%s?%s"
+	textHTML         = "text/html"
 )
 
 var (
-	Error502       = []byte(`<!DOCTYPE html>
+	debugMode  bool
+	remoteAddr string
+	remoteHost string
+)
+
+var (
+	error502       = []byte(`<!DOCTYPE html>
 <html>
 <head>
 <title>DoctError!</title>
@@ -56,22 +65,35 @@ Our servers ran into DoctError and are too frightened to continue.
 <br/>
 Help fight evil by waiting a moment and trying again. Thanks!
 </body></html>`)
-	Error502Length = fmt.Sprintf("%d", len(Error502))
+	error502Length = fmt.Sprintf("%d", len(error502))
 )
 
-var (
-	debugMode  bool
-	remoteAddr string
-	remoteHost string
-)
+type Redirector struct {
+	url string
+}
 
-type Proxy struct{}
+func (redirector *Redirector) ServeHTTP(conn *http.Conn, req *http.Request) {
 
-func serveError502(conn *http.Conn) {
-	conn.WriteHeader(502)
-	conn.SetHeader(ContentType, TextHTML)
-	conn.SetHeader(ContentLength, Error502Length)
-	conn.Write(Error502)
+	var url string
+	if len(req.URL.RawQuery) > 0 {
+		url = fmt.Sprintf(redirectURLQuery, redirector.url, req.URL.Path, req.URL.RawQuery)
+	} else {
+		url = fmt.Sprintf(redirectURL, redirector.url, req.URL.Path)
+	}
+
+	if len(url) == 0 {
+		url = "/"
+	}
+
+	conn.SetHeader("Location", url)
+	conn.WriteHeader(http.StatusMovedPermanently)
+	fmt.Fprintf(conn, redirectHTML, url)
+
+}
+
+
+type Proxy struct {
+	tlsMode bool
 }
 
 func (proxy *Proxy) ServeHTTP(conn *http.Conn, req *http.Request) {
@@ -135,39 +157,69 @@ func (proxy *Proxy) ServeHTTP(conn *http.Conn, req *http.Request) {
 
 }
 
+func serveError502(conn *http.Conn) {
+	conn.WriteHeader(http.StatusBadGateway)
+	conn.SetHeader(contentType, textHTML)
+	conn.SetHeader(contentLength, error502Length)
+	conn.Write(error502)
+}
+
 func main() {
 
-	opts := optparse.Parser("Usage: zeroproxy [options]\n", "zeroproxy 0.0.0")
+	opts := optparse.Parser(
+		"Usage: zeroproxy <zerolive-config.yaml> [options]\n",
+		"zeroproxy 0.0.0")
 
-	host := opts.String([]string{"--host"}, "localhost",
-		"the host to bind to [default: localhost]")
+	host := opts.String([]string{"--host"}, "",
+		"the host to bind the HTTPS server to", "HOST")
 
-	port := opts.Int([]string{"--port"}, 8010,
-		"the port to bind to [default: 8010]")
+	port := opts.Int([]string{"--port"}, 9443,
+		"the port to bind the HTTPS server to [default: 9443]", "PORT")
 
-	remoteHost := opts.String([]string{"--remote-host"}, "localhost",
-		"the remote host to connect to [default: localhost]")
+	certFile := opts.String([]string{"--cert-file"}, "",
+		"the path to the TLS certificate for the HTTPS server", "FILE")
 
-	remotePort := opts.Int([]string{"--remote-port"}, 8080,
-		"the remote port to connect to [default: 8080]")
+	httpHost := opts.String([]string{"--http-host"}, "",
+		"the host to bind the HTTP server to", "HOST")
 
-	tlsMode := opts.Bool([]string{"--tls"}, false,
-		"enable TLS (SSL) mode when connecting to the remote host")
+	httpPort := opts.Int([]string{"--http-port"}, 9080,
+		"the port to bind the HTTP server to [default: 9080]", "PORT")
 
-	debug := opts.Bool([]string{"--debug"}, false,
+	httpRedirect := opts.String([]string{"--redirect"},
+		"https://localhost:9443",
+		"the redirect for HTTP requests [default: https://localhost:9443]",
+		"URL")
+
+	remoteHost := opts.String([]string{"--gae-host"}, "localhost",
+		"the App Engine host to connect to [default: localhost]", "HOST")
+
+	remotePort := opts.Int([]string{"--gae-port"}, 8080,
+		"the App Engine port to connect to [default: 8080]", "PORT")
+
+	tlsMode := opts.Bool([]string{"--tls-proxy"}, false,
+		"proxy to App Engine using TLS [default: false]")
+
+	debug := opts.Bool([]string{"-d", "--debug"}, false,
 		"enable debug mode")
 
 	os.Args[0] = "zeroproxy"
 	args := opts.Parse(os.Args)
 
+	var liveConfig string
+
 	if len(args) >= 1 {
 		if args[0] == "help" {
 			opts.PrintUsage()
-			os.Exit(1)
+			os.Exit(0)
 		}
+		liveConfig = args[0]
+	} else {
+		opts.PrintUsage()
+		os.Exit(0)
 	}
 
-	_ = tlsMode
+	_ = liveConfig
+	_ = certFile
 
 	// Initialise the Ampify runtime -- which will run ``zeroproxy`` on multiple
 	// processors if possible.
@@ -178,18 +230,45 @@ func main() {
 
 	debugMode = *debug
 	remoteAddr = fmt.Sprintf("%s:%d", *remoteHost, *remotePort)
-	addr := fmt.Sprintf("%s:%d", *host, *port)
 
+	addr := fmt.Sprintf("%s:%d", *host, *port)
 	listener, err := net.Listen("tcp", addr)
 	if err != nil {
 		fmt.Printf("Cannot listen on %s: %v\n", addr, err)
 		os.Exit(1)
 	}
 
-	fmt.Printf("Running zeroproxy with %d CPUs on %s\n",
-		runtime.CPUCount, addr)
+	httpAddr := fmt.Sprintf("%s:%d", *httpHost, *httpPort)
+	httpListener, err := net.Listen("tcp", httpAddr)
+	if err != nil {
+		fmt.Printf("Cannot listen on %s: %v\n", httpAddr, err)
+		os.Exit(1)
+	}
 
-	proxy := &Proxy{}
+	var addrURL, httpAddrURL string
+
+	if len(*host) == 0 {
+		addrURL = fmt.Sprintf("https://localhost:%d", *port)
+	} else {
+		addrURL = fmt.Sprintf("https://%s:%d", *host, *port)
+	}
+
+	if len(*httpHost) == 0 {
+		httpAddrURL = fmt.Sprintf("http://localhost:%d", *httpPort)
+	} else {
+		httpAddrURL = fmt.Sprintf("http://%s:%d", *httpHost, *httpPort)
+	}
+
+	fmt.Printf("Running zeroproxy with %d CPUs:\n", runtime.CPUCount)
+	fmt.Printf("* Frontend proxy running on %s\n", addrURL)
+	fmt.Printf("* HTTP Redirector running on %s -> %s\n", httpAddrURL, *httpRedirect)
+
+	redirector := &Redirector{url: *httpRedirect}
+	http.Serve(httpListener, redirector)
+
+	proxy := &Proxy{
+		tlsMode: *tlsMode,
+	}
 	http.Serve(listener, proxy)
 
 }
