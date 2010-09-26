f1b9835960a041545f38c23db76dfb3dacd50331
tav <tav@espians.com>
2010-09-26 05:24:15 +0100

    Extended the pylibs and assetgen functionality.

--- environ/pylibs.py
+++ environ/pylibs.py
@@ -1,19 +1,20 @@
 # No Copyright (-) 2010 The Ampify Authors. This file is under the
 # Public Domain license that can be found in the root LICENSE file.
 
+import os
 import sys
 
 from os.path import dirname, join as join_path, realpath
 
 AMPIFY_ROOT = dirname(dirname(realpath(__file__)))
-PYUTIL_PATH = join_path(AMPIFY_ROOT, 'src')
+SRC_PATH = join_path(AMPIFY_ROOT, 'src')
 THIRD_PARTY_LIBS_PATH = join_path(AMPIFY_ROOT, 'third_party', 'pylibs')
-
-if PYUTIL_PATH not in sys.path:
-    sys.path.insert(0, PYUTIL_PATH)
 
 if THIRD_PARTY_LIBS_PATH not in sys.path:
     sys.path.insert(0, THIRD_PARTY_LIBS_PATH)
+
+if SRC_PATH not in sys.path:
+    sys.path.insert(0, SRC_PATH)
 
 try:
     import optcomplete
@@ -26,3 +27,5 @@ except ImportError:
     print "    git submodule update --init third_party/pylibs"
     print
     sys.exit(1)
+
+os.environ.setdefault('AMPIFY_ROOT', AMPIFY_ROOT)
