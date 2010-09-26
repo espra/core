078b938966d7fafb9d1bef91eb2375399b30939e
tav <tav@espians.com>
2010-09-26 07:00:24 +0100

    Restructured Zero.

--- src/template/ampnode/fabfile.example.py
+++
@@ -1,9 +0,0 @@
-# No Copyright (-) 2010 The Ampify Authors. This file is under the
-# Public Domain license that can be found in the root LICENSE file.
-
-def deploy():
-    """deploy ampify"""
-
-def run():
-    """run local version of ampify"""
-
