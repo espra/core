8d3be66f69e4e3d53b2a2c9b6656c851374bee5f
tav <tav@espians.com>
2010-12-11 00:07:28 +0000

    Implemented coffeescript compilation under Java.

--- src/cajoleit/com/espians/cajoleit/CajoleitServlet.java
+++ src/cajoleit/com/espians/cajoleit/CajoleitServlet.java
@@ -1,12 +1,50 @@
+// No Copyright (-) 2010 The Ampify Authors. This file is under the
+// Public Domain license that can be found in the root LICENSE file.
+
 package com.espians.cajoleit;
 
 import java.io.IOException;
 import javax.servlet.http.*;
 
+import org.mozilla.javascript.JavaScriptException;
+
+// -----------------------------------------------------------------------------
+// Servlet
+// -----------------------------------------------------------------------------
+
 public class CajoleitServlet extends HttpServlet {
+
+	private static CoffeeScript compiler = null;
+
+	public static CoffeeScript getCompiler() {
+		if (compiler == null) {
+			compiler = new CoffeeScript();
+		}
+		return compiler;
+	}
+
 	public void doGet(HttpServletRequest req, HttpServletResponse resp)
 			throws IOException {
 		resp.setContentType("text/plain");
 		resp.getWriter().println("Hello, world!");
 	}
+
+	public void doPost(HttpServletRequest req, HttpServletResponse resp)
+		throws IOException {
+
+		String source = req.getParameter("source");
+		String type = req.getParameter("type");
+		String compiledCoffee;
+
+		try {
+			compiledCoffee = getCompiler().compile(source);
+		} catch (JavaScriptException e) {
+			resp.getWriter().println(String.format("{\"error\": \"%s\"}", e.getValue().toString()));
+			return;
+		}
+
+		resp.setContentType("text/plain");
+		resp.getWriter().println(compiledCoffee);
+	}
+
 }
---
+++ src/cajoleit/com/espians/cajoleit/CoffeeScript.java
@@ -0,0 +1,80 @@
+// No Copyright (-) 2010 The Ampify Authors. This file is under the
+// Public Domain license that can be found in the root LICENSE file.
+
+package com.espians.cajoleit;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.io.UnsupportedEncodingException;
+
+import org.mozilla.javascript.Context;
+import org.mozilla.javascript.ContextFactory;
+import org.mozilla.javascript.JavaScriptException;
+import org.mozilla.javascript.Scriptable;
+
+// -----------------------------------------------------------------------------
+// CoffeeScript Compiler
+// -----------------------------------------------------------------------------
+
+public class CoffeeScript {
+
+	private static final String coffeeJS = "com/espians/cajoleit/coffee-script.js";
+	private static final String compileJS = "CoffeeScript.compile(source, {bare: true});";
+	private static final ContextFactory contextFactory = new ContextFactory();
+	private Scriptable parent;
+
+	public CoffeeScript() {
+
+		InputStream stream;
+		Reader reader;
+		Context ctx;
+
+		stream = getClass().getClassLoader().getResourceAsStream(coffeeJS);
+		try {
+			reader = new InputStreamReader(stream, "UTF-8");
+			ctx = contextFactory.enterContext();
+			ctx.setOptimizationLevel(-1);
+			try {
+				parent = ctx.initStandardObjects();
+				ctx.evaluateReader(parent, reader, "coffee-script.js", 1, null);
+			} catch (IOException e) {
+				throw new Error(e);
+			} finally {
+				Context.exit();
+				try {
+					reader.close();
+				} catch (IOException e) {
+					throw new Error(e);
+				}
+			}
+		} catch (UnsupportedEncodingException e) {
+			throw new Error(e);
+		} finally {
+			try {
+				stream.close();
+			} catch (IOException e) {
+				throw new Error(e);
+			}
+		}
+
+	}
+
+	public String compile (String source) {
+
+		Context ctx;
+		Scriptable scope;
+
+		ctx = contextFactory.enterContext();
+		try {
+			scope = ctx.newObject(parent);
+			scope.setParentScope(parent);
+			scope.put("source", scope, source);
+			return (String)ctx.evaluateString(scope, compileJS, "compilation", 1, null);
+		} finally {
+			Context.exit();
+		}
+
+	}
+}
