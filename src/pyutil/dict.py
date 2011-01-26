2a0f9a6b86eb53fc0514703f3360fb255767913f
tav <tav@espians.com>
2011-01-26 01:37:44 +0000

    Cleaned up CachingDict.

--- src/pyutil/dict.py
+++ src/pyutil/dict.py
@@ -13,8 +13,7 @@ class CachingDict(dict):
     """A caching dict that discards its least recently used items."""
 
     __slots__ = (
-        '_cache_size', '_garbage_collector', '_buffer_size', 'itersort',
-        '_clock'
+        '_buffer_size', '_cache_size', '_clock', '_garbage_collector'
         )
 
     def __init__(
@@ -22,10 +21,14 @@ class CachingDict(dict):
         **kwargs
         ):
 
+        if buffer_size is None:
+            self._buffer_size = cache_size / 2
+        else:
+            self._buffer_size = buffer_size
+
         self._cache_size = cache_size
-        self._garbage_collector = garbage_collector
-        self._buffer_size = buffer_size or cache_size / 2
         self._clock = 0
+        self._garbage_collector = garbage_collector
 
         for key, value in args:
             self.__setitem__(key, value)
@@ -36,16 +39,17 @@ class CachingDict(dict):
     def __setitem__(self, key, value):
         excess = len(self) - self._cache_size - self._buffer_size + 1
         if excess > 0:
-            garbage_collector = self._garbage_collector
-            # @@ time against : heapq.nsmallest()
             excess = sorted(self.itersort())[:excess + self._buffer_size]
-            for ex_value, ex_key in excess:
-                if garbage_collector:
+            garbage_collector = self._garbage_collector
+            if garbage_collector:
+                for ex_value, ex_key in excess:
                     garbage_collector(ex_key, ex_value)
-                del self[ex_key]
-
+                    del self[ex_key]
+            else:
+                for _, ex_key in excess:
+                    del self[ex_key]
         self._clock += 1
-        return dict.__setitem__(self, key, [self._clock, value])
+        dict.__setitem__(self, key, [self._clock, value])
 
     def __getitem__(self, key):
         if key in self:
@@ -53,7 +57,6 @@ class CachingDict(dict):
             self._clock += 1
             access[0] = self._clock
             return access[1]
-
         raise KeyError(key)
 
     def itersort(self):
@@ -67,15 +70,12 @@ class CachingDict(dict):
         return default
 
     def pop(self, key, default=Blank):
-
         if key in self:
             value = dict.__getitem__(self, key)[1]
             del self[key]
             return value
-
         if default is not Blank:
             return default
-
         raise KeyError(key)
 
     def setdefault(self, key, default):
@@ -84,13 +84,17 @@ class CachingDict(dict):
         self.__setitem__(key, default)
         return default
 
+    iterkeys = dict.iterkeys
+    keys = dict.keys
+
     def itervalues(self):
         getitem = self.__getitem__
         for key in self:
             yield getitem(key)
 
     def values(self):
-        return list(self.itervalues())
+        getitem = self.__getitem__
+        return [getitem(key) for key in self]
 
     def iteritems(self):
         getitem = self.__getitem__
@@ -98,11 +102,14 @@ class CachingDict(dict):
             yield key, getitem(key)
 
     def items(self):
-        return list(self.iteritems())
+        getitem = self.__getitem__
+        return [(key, getitem(key)) for key in self]
 
-    def set_cache_size(self, cache_size):
-        if not isinstance(cache_size, (int, long)):
-            raise ValueError("Cache size must be an integer.")
+    def set_cache_size(self, cache_size, buffer_size=None):
+        if buffer_size is None:
+            self._buffer_size = cache_size / 2
+        else:
+            self._buffer_size = buffer_size
         self._cache_size = cache_size
 
     def get_cache_byte_size(self):
