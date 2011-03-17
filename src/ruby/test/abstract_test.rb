8772ffc79f14fbbf465edb99904e4bead8b2ece8
tav <tav@espians.com>
2011-03-17 10:19:01 +0000

    Added base files for a Ruby implementation.

---
+++ src/ruby/test/abstract_test.rb
@@ -0,0 +1,10 @@
+# Public Domain (-) 2011 The Ampify Authors.
+# See the Ampify UNLICENSE file for details.
+
+require 'rubygems'
+require 'test/unit'
+require 'shoulda'
+
+$LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
+
+load 'ampify.rb'
---
+++ src/ruby/test/nodule_test.rb
@@ -0,0 +1,4 @@
+# Public Domain (-) 2011 The Ampify Authors.
+# See the Ampify UNLICENSE file for details.
+
+require 'abstract_test'
