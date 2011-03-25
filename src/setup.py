ff29b64c8902720720aaa72e79680d9eed325993
tav <tav@espians.com>
2011-03-25 02:36:40 +0000

    Updated setup.py for the Cython extensions.

--- src/setup.py
+++ src/setup.py
@@ -3,40 +3,39 @@
 # Public Domain (-) 2010-2011 The Ampify Authors.
 # See the Ampify UNLICENSE file for details.
 
-import pyutil
 import sys
 
 from distutils.core import Extension, setup
 from Cython.Distutils import build_ext
 
 # ------------------------------------------------------------------------------
-# the extensions
+# Extensions
 # ------------------------------------------------------------------------------
 
 extensions = [
     Extension(
-        "pyutil.lzf",
-        ["pyutil/lzf.pyx", "pyutil/lzf/lzf_c.c", "pyutil/lzf/lzf_d.c"],
-        include_dirs=["pyutil/lzf"],
+        "ampify._lzf",
+        ["ampify/_lzf.pyx", "ampify/liblzf/lzf_c.c", "ampify/liblzf/lzf_d.c"],
+        include_dirs=["ampify/liblzf"],
         )
     ]
 
 if sys.platform == 'darwin':
     extensions.append(
-        Extension("pyutil.darwinsandbox", ["pyutil/darwinsandbox.pyx"])
+        Extension("ampify.darwinsandbox", ["ampify/darwinsandbox.pyx"])
         )
 
 # ------------------------------------------------------------------------------
-# run setup
+# Run Setup
 # ------------------------------------------------------------------------------
 
 if not sys.argv[1:]:
     sys.argv.extend(['build_ext', '-i'])
 
 setup(
-    name="pyutil",
+    name="ampify",
     version="git",
-    description="Pyutil: A collection of useful Python modules",
+    description="Ampify: A decentralised social platform",
     cmdclass=dict(build_ext=build_ext),
     ext_modules=extensions,
     )
