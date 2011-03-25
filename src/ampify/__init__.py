ac63b352e56d421bf9e02a13e51867a68c21523e
tav <tav@espians.com>
2011-03-25 06:07:40 +0000

    Updated ampify/__init__.py to be more informative.

---
+++ src/ampify/__init__.py
@@ -0,0 +1,34 @@
+# Public Domain (-) 2010-2011 The Ampify Authors.
+# See the Ampify UNLICENSE file for details.
+
+"""
+======
+Ampify
+======
+
+Ampify is an open and decentralised social platform. It provides an application
+framework to create social apps on top of a secure, decentralised core.
+
+::
+
+                                   ___       
+                              _  /'___)      
+       _ _   ___ ___   _ _   (_)| (__  _   _ 
+     /'_` )/' _ ` _ `\( '_`\ | || ,__)( ) ( )
+    ( (_| || ( ) ( ) || (_) )| || |   | (_) |
+    `\__,_)(_) (_) (_)| ,__/'(_)(_)   `\__, |
+                      | |             ( )_| |
+                      (_)             `\___/'
+
+"""
+
+# ------------------------------------------------------------------------------
+# Metadata
+# ------------------------------------------------------------------------------
+
+__version__ = (0, 0, 0)
+__release__ = '.'.join(map(str, __version__))
+
+__authors__ = {
+    0x01: ('tav', 'tav@espians.com')
+    }
