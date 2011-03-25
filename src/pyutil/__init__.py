33431e8a3148f85d0f78b3b2b22bfcef7a0da5bf
tav <tav@espians.com>
2011-03-25 02:30:46 +0000

    Updated pyutil to only extend sys.path conditionally.

--- src/pyutil/__init__.py
+++ src/pyutil/__init__.py
@@ -17,21 +17,18 @@ Pyutil -- A collection of utility modules for Python.
 
 import sys
 
-from os.path import dirname, join as join_path, realpath
-
-# ------------------------------------------------------------------------------
-# extend sys.path to include the ``third_party`` lib direktory
-# ------------------------------------------------------------------------------
+from os.path import dirname, isdir, join as join_path, realpath
 
 def extend_sys_path():
+    """Extend ``sys.path`` to include the third-party ``pylibs`` directory."""
 
     THIRD_PARTY_LIBS_PATH = join_path(
-        dirname(dirname(realpath(__file__))), 'third_party', 'pylibs'
+        dirname(dirname(dirname(realpath(__file__)))), 'third_party', 'pylibs'
         )
 
-    if THIRD_PARTY_LIBS_PATH not in sys.path:
-        sys.path.insert(0, THIRD_PARTY_LIBS_PATH)
-
-    return THIRD_PARTY_LIBS_PATH
+    if isdir(THIRD_PARTY_LIBS_PATH):
+        if THIRD_PARTY_LIBS_PATH not in sys.path:
+            sys.path.insert(0, THIRD_PARTY_LIBS_PATH)
+        return THIRD_PARTY_LIBS_PATH
 
 extend_sys_path()
