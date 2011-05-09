e4586a3e7d7d58471fd01a04b7fe32f5f91914b6
tav <tav@espians.com>
2011-05-10 00:11:54 +0100

    Migrated webkit_bridge to the Naaga repo.

--- src/webkit_bridge/__init__.py
+++
@@ -1,4 +0,0 @@
-# Public Domain (-) 2009-2011 The Ampify Authors.
-# See the Ampify UNLICENSE file for details.
-
-"""A WebKit bridge for PyPy-based interpreters.""" 
--- src/webkit_bridge/conftest.py
+++
@@ -1,4 +0,0 @@
-# Public Domain (-) 2009-2011 The Ampify Authors.
-# See the Ampify UNLICENSE file for details.
-
-from pypy.conftest import Directory, Module, option, ConftestPlugin
