f4809fb9936d1f998773876eb94202ca7d1006e3
tav <tav@espians.com>
2011-07-17 02:22:34 +0100

    Added a utility amp/dict package.

---
+++ src/amp/dict/dict.go
@@ -0,0 +1,19 @@
+// Public Domain (-) 2011 The Ampify Authors.
+// See the Ampify UNLICENSE file for details.
+
+package dict
+
+import (
+	"sort"
+)
+
+func SortedKeys(dict map[string]string) (keys []string) {
+	keys = make([]string, len(dict))
+	i := 0
+	for key, _ := range dict {
+		keys[i] = key
+		i += 1
+	}
+	sort.StringSlice(keys).Sort()
+	return
+}
---
+++ src/amp/dict/dict_test.go
@@ -0,0 +1,26 @@
+// Public Domain (-) 2011 The Ampify Authors.
+// See the Ampify UNLICENSE file for details.
+
+package dict
+
+import (
+	"testing"
+)
+
+func TestSortedKeys(t *testing.T) {
+	dict := map[string]string{
+		"tav": "espian",
+		"james": "arthur",
+		"sofia": "bustamante",
+		"mamading": "ceesay",
+	}
+	keys := SortedKeys(dict)
+	expected := []string{"james", "mamading", "sofia", "tav"}
+	for idx, key := range keys {
+		if key != expected[idx] {
+			t.Errorf("Unexpected item at position %d: %s (expected: %s)",
+				idx, key, expected[idx])
+		}
+	}
+
+}
