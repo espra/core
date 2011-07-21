e75cc44c2cbac7e464fdeb3c228acd9919525931
tav <tav@espians.com>
2011-07-21 03:03:22 +0100

    Added stub commands for the amp runtime.

--- src/amp/amp/amp.go
+++ src/amp/amp/amp.go
@@ -5,33 +5,39 @@ package main
 
 import (
 	"amp/optparse"
-	"amp/runtime"
-	"fmt"
-	"os"
 )
 
 func main() {
 
-	opts := optparse.Parser(
-		"Usage: amp <command> [options]\n",
-		"amp 0.0.0")
-
-	debug := opts.Bool([]string{"-d", "--debug"}, false,
-		"enable debug mode")
-
-	os.Args[0] = "amp"
-	args := opts.Parse(os.Args)
-
-	// Initialise the Ampify runtime -- which will run ``amp`` on multiple
-	// processors if possible.
-	runtime.Init()
-
-	if *debug {
-		fmt.Println("DEBUG: enabled")
+	commands := map[string]func([]string, string){
+		"build":    build,
+		"frontend": frontend,
+		"init":     initialise,
+		"node":     node,
+		"pull":     pull,
+		"push":     push,
+		"repo":     repo,
+		"review":   review,
+		"run":      run,
+		"store":    store,
+		"test":     test,
 	}
 
-	if len(args) > 0 {
-		fmt.Printf("COMMAND: %v\n", args)
+	usage := map[string]string{
+		"build":    "build an amp nodule",
+		"frontend": "run an amp frontend",
+		"init":     "initialise an instance",
+		"node":     "run an amp node",
+		"pull":     "pull from an amp repo",
+		"push":     "push to an amp repo",
+		"repo":     "run an amp repo",
+		"review":   "review an amp repo",
+		"run":      "run a combined single-server instance",
+		"store":    "run an amp store",
+		"test":     "test an amp nodule",
+		"version":  "show the version number and exit",
 	}
-	
+
+	optparse.Subcommands("amp", "amp 0.0.0", commands, usage)
+
 }
---
+++ src/amp/amp/repo.go
@@ -0,0 +1,28 @@
+// Public Domain (-) 2011 The Ampify Authors.
+// See the Ampify UNLICENSE file for details.
+
+package main
+
+func build(argv []string, usage string) {
+}
+
+func initialise(argv []string, usage string) {
+}
+
+func pull(argv []string, usage string) {
+}
+
+func push(argv []string, usage string) {
+}
+
+func repo(argv []string, usage string) {
+}
+
+func review(argv []string, usage string) {
+}
+
+func store(argv []string, usage string) {
+}
+
+func test(argv []string, usage string) {
+}
