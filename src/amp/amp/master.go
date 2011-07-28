a97950fbed76ceb5ae81fa8d9d72f0edf9226aa8
tav <tav@espians.com>
2011-07-28 01:24:35 +0100

    Renamed repo node -> master node.

---
+++ src/amp/amp/master.go
@@ -0,0 +1,13 @@
+// Public Domain (-) 2011 The Ampify Authors.
+// See the Ampify UNLICENSE file for details.
+
+package main
+
+func ampMaster(argv []string, usage string) {
+}
+
+func ampPull(argv []string, usage string) {
+}
+
+func ampPush(argv []string, usage string) {
+}
