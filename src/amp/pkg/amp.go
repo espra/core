4a8443abc566c6d2397e2422ae48e1547dfd5ae2
tav <tav@espians.com>
2011-10-24 00:19:50 +0100

    Added stubs for amp.Context.

--- src/amp/pkg/amp.go
+++ src/amp/pkg/amp.go
@@ -3,23 +3,89 @@
 
 package amp
 
+import (
+	"amp/rpc"
+	"io"
+	"os"
+)
+
 type Context struct {
-	Header
+	// Error
+	Header         rpc.Header
+	ResponseHeader rpc.Header
+	Streams        []io.ReadCloser
+}
+
+func (ctx *Context) Call(method string, args ...interface{}) (req *Request) {
+	return
+}
+
+func (ctx *Context) Dial(identity string) (req *Request) {
+	return
+}
+
+func (ctx *Context) DialHTTPS(url string) (req *Request) {
+	return
+}
+
+func (ctx *Context) Error() {
+}
+
+func (ctx *Context) SetStream(stream io.ReadCloser) {
+	if ctx.Streams == nil {
+		ctx.Streams = []io.ReadCloser{stream}
+	} else {
+		ctx.Streams = append(ctx.Streams, stream)
+	}
 }
 
 type Request struct {
-	Header
+	Header rpc.Header
+}
+
+func (req *Request) Return(vals ...interface{}) (err os.Error) {
+	return nil
 }
 
 func Call() {
-
 }
 
 func Register() {
-
 }
 
 // func Hello(ctx *web.Context, files *web.Files) {
 // }
 
 // web.GetCookie(ctx)
+
+// func (db *DB) CreateItem(ctx, user string, item *Item) {
+// 	if !db.CheckUser(ctx, user) {
+// 		return
+// 	}
+// 	key = NewKey(parent=user)
+// 	db.Put(key, item)
+// }
+
+// func (db *DB) CheckUser(ctx, user) (ok bool) {
+// 	if ctx.Header.GetString("__user") != user {
+// 		ctx.Error("AuthError")
+// 		return false
+// 	}
+// }
+
+// in nodule memcache:
+
+// func (api *API) Get(ctx, key string) {
+// 	key = Namespace(ctx, key)
+// 	return ctx.Call(memcacheRef, "memcache.Get", key)
+// }
+
+// in nodule memcacheRef:
+
+// func (cache *MemCache) Get(ctx, key string) {
+// 	return cache.values[key]
+// }
+
+// Register(&Memcache{})
+// Register(&Facebook{Secret: "SECRET", AppID: "Blah"})
+// RegisterName("fb", &Facebook{Secret: "SECRET", AppID: "Blah"})
