173b3f6473e13325751719d1f453d8ecb9cd2604
Seyi Ogunyemi <micrypt@gmail.com>
2011-11-08 15:13:07 +0000

    buildrecipes: fixed OpenSSL 64 bit build issues

--- environ/redpill.py
+++ environ/redpill.py
@@ -897,7 +897,7 @@ def cleanup_install():
 # Virgin Build Handler
 # ------------------------------------------------------------------------------
 
-VIRGIN_BUILD = not exists(join(LOCAL, 'bin', 'bsdiff'))
+VIRGIN_BUILD = not exists(join(LOCAL, 'bin', 'python'))
 
 def build_base_and_reload():
     load_role('base')
