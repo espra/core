4401a9e093ead9a2f6fbe9cad7c34399057350ad
tav <tav@espians.com>
2012-08-10 18:26:19 +0100

    amp/db: started reworking db.Client to use zerodata.Client.

--- src/amp/db/client.go
+++ src/amp/db/client.go
@@ -1,101 +1,51 @@
-// Public Domain (-) 2011 The Ampify Authors.
+// Public Domain (-) 2011-2012 The Ampify Authors.
 // See the Ampify UNLICENSE file for details.
 
-package main
+package db
 
 import (
-	"amp/model"
-	"bytes"
-	"encoding/gob"
-	"fmt"
-	"io"
-	"log"
-	"net/http"
-	"net/rpc"
-	"os"
+	"amp/zerodata"
 	"sync"
 )
 
-// const url = "http://localhost:8080/?key=blah"
-const url = "https://togethrat.appspot.com/?key=blah"
+var (
+	clients       []*client
+	defaultSecret []byte
+	defaultURL    string
+	mutex         sync.Mutex
+)
 
-type Codec struct {
-	m      sync.Mutex
-	client *http.Client
-	enc    *gob.Encoder
-	encBuf *bytes.Buffer
-	err    bool
-	resp   io.ReadCloser
-	dec    *gob.Decoder
+type client struct {
+	impl *zerodata.Client
 }
 
-func (c *Codec) WriteRequest(r *rpc.Request, body interface{}) (err os.Error) {
-	defer c.m.Unlock()
-	if err = c.enc.Encode(r); err != nil {
-		c.err = true
-		return
-	}
-	if err = c.enc.Encode(body); err != nil {
-		c.err = true
-		return
-	}
-	resp, err := c.client.Post(url, "raw", c.encBuf)
-	if err != nil {
-		c.err = true
-		return
-	}
-	c.resp = resp.Body
-	c.dec = gob.NewDecoder(resp.Body)
-	return nil
+func (c *client) Get() {
+
 }
 
-func (c *Codec) ReadResponseHeader(r *rpc.Response) os.Error {
-	c.m.Lock()
-	defer c.m.Unlock()
-	if c.err {
-		return os.EOF
-	}
-	return c.dec.Decode(r)
+func (c *client) Release() {
+	mutex.Lock()
+	clients = append(clients, c)
+	mutex.Unlock()
 }
 
-func (c *Codec) ReadResponseBody(body interface{}) os.Error {
-	if c.err {
-		return os.EOF
+func Client() *client {
+	mutex.Lock()
+	defer mutex.Unlock()
+	i := len(clients) - 1
+	if i >= 0 {
+		c := clients[i]
+		clients = clients[:i]
+		return c
 	}
-	return c.dec.Decode(body)
+	return &client{zerodata.NewClient(defaultSecret, defaultURL)}
 }
 
-func (c *Codec) Close() os.Error {
-	return c.resp.Close()
+func NewClientWithOpts(secret []byte, url string) *client {
+	return &client{zerodata.NewClient(secret, url)}
 }
 
-func Dial() *rpc.Client {
-	httpClient := &http.Client{}
-	buf := &bytes.Buffer{}
-	codec := &Codec{
-		client: httpClient,
-		enc:    gob.NewEncoder(buf),
-		encBuf: buf,
-	}
-	codec.m.Lock()
-	return rpc.NewClientWithCodec(codec)
-}
-
-func main() {
-
-	client := Dial()
-
-	item := &model.Item{
-		Parent1: "boo",
-	}
-
-	var key string
-
-	err := client.Call("db.CreateItem", item, &key)
-	if err != nil {
-		log.Fatal("Error:", err)
-	}
-
-	fmt.Printf("Key: %v\n", key)
-
+func SetDefaultOpts(secret []byte, url string) {
+	defaultSecret = secret
+	defaultURL = url
 }
