3a3c5c9170f56a0c8682adcec6165f2030fd43d0
tav <tav@espians.com>
2012-08-10 04:35:02 +0100

    zerodata: implemented basic get/put/delete proxying.

--- src/zerodata/app/zerodata.go
+++ src/zerodata/app/zerodata.go
@@ -5,11 +5,92 @@ package zerodata
 
 import (
 	"appengine"
+	"appengine/datastore"
 	"crypto/subtle"
+	"dbi"
 	"fmt"
 	"net"
 	"net/http"
 )
+
+func getKey(ctx appengine.Context, key *dbi.Key) *datastore.Key {
+	var parent *datastore.Key
+	if key.Parent != nil {
+		parent = getKey(ctx, key.Parent)
+	}
+	return datastore.NewKey(ctx, key.Kind, key.StringID, key.IntID, parent)
+}
+
+func retKey(key *datastore.Key) *dbi.Key {
+	return &dbi.Key{
+		Kind:     key.Kind(),
+		StringID: key.StringID(),
+		IntID:    key.IntID(),
+		Parent:   retKey(key.Parent()),
+	}
+}
+
+func AllocateIDs(ctx appengine.Context, req *dbi.AllocRequest, alloc *dbi.Allocation) error {
+	low, high, err := datastore.AllocateIDs(ctx, req.Kind, getKey(ctx, req.Parent), req.Amount)
+	if err != nil {
+		return err
+	}
+	alloc.Low = low
+	alloc.High = high
+	return nil
+}
+
+func Delete(ctx appengine.Context, key *dbi.Key, ok bool) error {
+	return datastore.Delete(ctx, getKey(ctx, key))
+}
+
+func DeleteMulti(ctx appengine.Context, keys []*dbi.Key, ok bool) error {
+	xkeys := make([]*datastore.Key, len(keys))
+	for idx, key := range keys {
+		xkeys[idx] = getKey(ctx, key)
+	}
+	return datastore.DeleteMulti(ctx, xkeys)
+}
+
+func Get(ctx appengine.Context, key *dbi.Key, entity *dbi.Entity) error {
+	return datastore.Get(ctx, getKey(ctx, key), entity)
+}
+
+func GetMulti(ctx appengine.Context, keys []*dbi.Key, entities *dbi.EntityList) error {
+	xkeys := make([]*datastore.Key, len(keys))
+	for idx, key := range keys {
+		xkeys[idx] = getKey(ctx, key)
+	}
+	return datastore.GetMulti(ctx, xkeys, entities.List)
+}
+
+func Put(ctx appengine.Context, kv *dbi.KeyValue, rkey *dbi.Key) error {
+	key, err := datastore.Put(ctx, getKey(ctx, kv.Key), &kv.Value)
+	if err != nil {
+		return err
+	}
+	rkey = retKey(key)
+	return nil
+}
+
+func PutMulti(ctx appengine.Context, kvlist []*dbi.KeyValue, rkeylist *dbi.KeyList) error {
+	keys := make([]*datastore.Key, len(kvlist))
+	entities := make([]dbi.Entity, len(kvlist))
+	for idx, kv := range kvlist {
+		keys[idx] = getKey(ctx, kv.Key)
+		entities[idx] = kv.Value
+	}
+	keys, err := datastore.PutMulti(ctx, keys, entities)
+	if err != nil {
+		return err
+	}
+	rkeys := make([]*dbi.Key, len(keys))
+	for idx, key := range keys {
+		rkeys[idx] = retKey(key)
+	}
+	rkeylist.List = rkeys
+	return nil
+}
 
 func handler(w http.ResponseWriter, r *http.Request) {
 	defer r.Body.Close()
@@ -67,11 +148,21 @@ func raise(status int, ctx appengine.Context, w http.ResponseWriter, s string, a
 }
 
 func init() {
+	// Validate the secret key settings.
 	if secretLength == 0 {
 		panic("the secret key cannot be empty.")
 	}
 	if secretLength != len(secret) {
 		panic("the secret key length does not match the `secretLength` variable.")
 	}
+	// Register RPC services.
+	Register("allocate-ids", AllocateIDs)
+	Register("delete", Delete)
+	Register("delete-multi", DeleteMulti)
+	Register("get", Get)
+	Register("get-multi", GetMulti)
+	Register("put", Put)
+	Register("put-multi", PutMulti)
+	// Register the root HTTP handler.
 	http.HandleFunc("/", handler)
 }
