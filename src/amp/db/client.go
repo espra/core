472c2cb0f5ce6d7a7b96f1cb83dde8e255717d8f
tav <tav@espians.com>
2012-08-15 15:21:14 +0100

    amp/db: required amp.Context to be passed into the client constructor.

--- src/amp/db/client.go
+++ src/amp/db/client.go
@@ -4,45 +4,60 @@
 package db
 
 import (
+	"amp"
 	"amp/zerodata"
 	"sync"
 )
 
 var (
-	clients       []*client
+	clients       []*Client
 	defaultSecret []byte
 	defaultURL    string
 	mutex         sync.Mutex
 )
 
-type client struct {
+type Client struct {
+	ctx  *amp.Context
 	impl *zerodata.Client
+	rel  bool
 }
 
-func (c *client) Get() {
+func (c *Client) Get() {
 
 }
 
-func (c *client) Release() {
+func (c *Client) Release() {
+	if !c.rel {
+		return
+	}
 	mutex.Lock()
 	clients = append(clients, c)
 	mutex.Unlock()
 }
 
-func Client() *client {
+func NewClient(ctx *amp.Context) *Client {
 	mutex.Lock()
 	defer mutex.Unlock()
 	i := len(clients) - 1
 	if i >= 0 {
 		c := clients[i]
 		clients = clients[:i]
+		c.ctx = ctx
 		return c
 	}
-	return &client{zerodata.NewClient(defaultSecret, defaultURL)}
+	return &Client{
+		ctx:  ctx,
+		impl: zerodata.NewClient(defaultSecret, defaultURL),
+		rel:  true,
+	}
 }
 
-func NewClientWithOpts(secret []byte, url string) *client {
-	return &client{zerodata.NewClient(secret, url)}
+func NewClientWithOpts(ctx *amp.Context, secret []byte, url string) *Client {
+	return &Client{
+		ctx:  ctx,
+		impl: zerodata.NewClient(secret, url),
+		rel:  false,
+	}
 }
 
 func SetDefaultOpts(secret []byte, url string) {
