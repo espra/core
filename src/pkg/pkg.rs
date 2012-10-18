2b6509bdb83e0cceb76ed765ff981df278025334
tav <tav@espians.com>
2012-10-18 17:43:37 +0100

    pkg: add the top-level definition for the pkg crate.

---
+++ src/pkg/pkg.rs
@@ -0,0 +1,10 @@
+// Public Domain (-) 2012 The Ampify Authors.
+// See the Ampify UNLICENSE file for details.
+
+//= version: 0.1
+//= An extended standard library for the Rust programming language.
+
+extern mod std(vers = "0.5");
+
+// The builtin `u8` type is aliased to the more familiar `byte` type.
+type byte = u8;
