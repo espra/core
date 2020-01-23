915fce2b9c04e049cbaaec2a17b19fd09365e5de
tav <tav@dappui.com>
2020-01-23 17:37:30 +0000

    pkg/mock: add a mock of os.Exit for testing purposes

---
+++ pkg/mock/osexit/osexit_test.go
@@ -0,0 +1,38 @@
+// Public Domain (-) 2018-present, The Core Authors.
+// See the Core UNLICENSE file for details.
+
+package osexit_test
+
+import (
+	"os"
+	"testing"
+
+	"espra.com/pkg/mock/osexit"
+)
+
+var osExit = os.Exit
+
+func TestOsExit(t *testing.T) {
+	osExit = osexit.Set()
+	osExit(2)
+	if !osexit.Called() {
+		t.Fatalf("mock exit function was not called")
+	}
+	status := osexit.Status()
+	if status != 2 {
+		t.Fatalf("mock exit function did not set the right status code: got %d, want 2", status)
+	}
+	osExit(3)
+	status = osexit.Status()
+	if status != 2 {
+		t.Fatalf("mock exit function overrode the status set by a previous call: got %d, want 2", status)
+	}
+	osexit.Reset()
+	if osexit.Called() {
+		t.Fatalf("the reset mock exit function claims to have been called")
+	}
+	status = osexit.Status()
+	if status != 0 {
+		t.Fatalf("the reset mock exit function returned a non-zero status code: got %d, want 0", status)
+	}
+}
