a06c82261696ee0a00a0fdc0ab96710a42a4a913
tav <tav@dappui.com>
2020-01-27 20:51:40 +0000

    infra/terraform: enable some base google cloud services

--- infra/terraform/base/main.tf
+++ infra/terraform/base/main.tf
@@ -7,6 +7,7 @@ terraform {
 }
 
 provider "google" {
+  project = var.base_project_id
   version = "= 3.5.0"
 }
 
@@ -20,4 +21,24 @@ resource "google_project" "base" {
   folder_id       = google_folder.core.name
   name            = "DappUI"
   project_id      = var.base_project_id
+}
+
+resource "google_project_service" "base_compute" {
+  service = "compute.googleapis.com"
+}
+
+resource "google_project_service" "base_datastore" {
+  service = "datastore.googleapis.com"
+}
+
+resource "google_project_service" "base_firestore" {
+  service = "firestore.googleapis.com"
+}
+
+resource "google_project_service" "base_storage" {
+  service = "storage-component.googleapis.com"
+}
+
+resource "google_project_service" "base_storage_api" {
+  service = "storage-api.googleapis.com"
 }
