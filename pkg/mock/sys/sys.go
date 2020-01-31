fa0f11d30c91a634eb727b9b3136eef07a8f7ce4
tav <tav@dappui.com>
2020-01-31 04:04:03 +0000

    pkg/mock: add mock for interactions with the filesystem

---
+++ pkg/mock/sys/sys.go
@@ -0,0 +1,262 @@
+// Public Domain (-) 2020-present, The Core Authors.
+// See the Core UNLICENSE file for details.
+
+// Package sys mocks interactions with the operating system.
+package sys
+
+import (
+	"bytes"
+	"errors"
+	"fmt"
+	"os"
+	"path/filepath"
+	"sort"
+	"strings"
+	"sync"
+	"time"
+
+	"dappui.com/pkg/sys"
+)
+
+var (
+	errCloseFailure = errors.New("sys: failed to close file")
+	errReadFailure  = errors.New("sys: failed to read file")
+	errStatFailure  = errors.New("sys: failed to stat file")
+)
+
+// File provides a mock implementation of the sys.File interface.
+type File struct {
+	closed    bool
+	data      *bytes.Buffer
+	failClose bool
+	failRead  bool
+	mu        sync.Mutex // protects closed
+}
+
+// Close implements the interface for sys.File.
+func (f *File) Close() error {
+	f.mu.Lock()
+	defer f.mu.Unlock()
+	if f.closed {
+		return os.ErrClosed
+	}
+	if f.failClose {
+		return errCloseFailure
+	}
+	f.closed = true
+	return nil
+}
+
+// Read implements the interface for sys.File.
+func (f *File) Read(p []byte) (n int, err error) {
+	f.mu.Lock()
+	defer f.mu.Unlock()
+	if f.closed {
+		return 0, os.ErrClosed
+	}
+	if f.failRead {
+		return 0, errReadFailure
+	}
+	return f.data.Read(p)
+}
+
+// FileInfo provides a mock implementation of the os.FileInfo interface.
+type FileInfo struct {
+	data      string
+	dir       bool
+	failClose bool
+	failRead  bool
+	failStat  bool
+	name      string
+}
+
+// FailClose will mark a file to fail when its Close method is called.
+func (f *FileInfo) FailClose() *FileInfo {
+	f.failClose = true
+	return f
+}
+
+// FailRead will mark a file to fail when its Read method is called.
+func (f *FileInfo) FailRead() *FileInfo {
+	f.failRead = true
+	return f
+}
+
+// FailStat will return an error for Lstat/Stat calls at the current path.
+func (f *FileInfo) FailStat() *FileInfo {
+	f.failStat = true
+	return f
+}
+
+// IsDir implements the interface for os.FileInfo.
+func (f *FileInfo) IsDir() bool {
+	return f.dir
+}
+
+// ModTime implements the interface for os.FileInfo.
+func (f *FileInfo) ModTime() time.Time {
+	return time.Time{}
+}
+
+// Mode implements the interface for os.FileInfo.
+func (f *FileInfo) Mode() os.FileMode {
+	if f.dir {
+		return os.ModeDir
+	}
+	return 0
+}
+
+// Name implements the interface for os.FileInfo.
+func (f *FileInfo) Name() string {
+	return f.name
+}
+
+// Size implements the interface for os.FileInfo.
+func (f *FileInfo) Size() int64 {
+	return int64(len(f.data))
+}
+
+// Sys implements the interface for os.FileInfo.
+func (f *FileInfo) Sys() interface{} {
+	return f.name
+}
+
+// FileSystem provides a configurable mock implementation of the sys.FileSystem
+// interface.
+type FileSystem struct {
+	files map[string]*FileInfo
+	mu    sync.RWMutex // protects files
+}
+
+// Lstat implements the interface for sys.Filesystem.
+func (f *FileSystem) Lstat(path string) (os.FileInfo, error) {
+	return f.Stat(path)
+}
+
+// Mkdir creates a directory at the given path, along with any necessary parent
+// directories. If a directory already exists, then the method will exit early.
+//
+// WriteFile implicitly creates directories, so this method is only really
+// useful for creating empty directories.
+func (f *FileSystem) Mkdir(path string) *FileInfo {
+	f.mu.Lock()
+	defer f.mu.Unlock()
+	path = filepath.Clean("/" + path)
+	if info, ok := f.files[path]; ok {
+		return info
+	}
+	dir, name := filepath.Split(path)
+	info := &FileInfo{
+		dir:  true,
+		name: name,
+	}
+	fmt.Printf("CREATING DIR %q\n", path)
+	f.files[path] = info
+	path = dir
+	for path != "/" {
+		path = filepath.Clean(path)
+		if _, ok := f.files[path]; ok {
+			break
+		}
+		dir, name := filepath.Split(path)
+		fmt.Printf("CREATING DIR %q\n", path)
+		f.files[path] = &FileInfo{
+			dir:  true,
+			name: name,
+		}
+		path = dir
+	}
+	return info
+}
+
+// Open implements the interface for sys.Filesystem.
+func (f *FileSystem) Open(path string) (sys.File, error) {
+	f.mu.RLock()
+	defer f.mu.RUnlock()
+	info, ok := f.files[path]
+	if !ok {
+		return nil, os.ErrNotExist
+	}
+	if info.failStat {
+		return nil, errStatFailure
+	}
+	return &File{
+		data:      bytes.NewBufferString(info.data),
+		failClose: info.failClose,
+		failRead:  info.failRead,
+	}, nil
+}
+
+// Stat implements the interface for sys.Filesystem.
+func (f *FileSystem) Stat(path string) (os.FileInfo, error) {
+	f.mu.RLock()
+	info, ok := f.files[path]
+	f.mu.RUnlock()
+	if !ok {
+		return nil, os.ErrNotExist
+	}
+	if info.failStat {
+		return nil, errStatFailure
+	}
+	return info, nil
+}
+
+// Walk implements the interface for sys.Filesystem.
+func (f *FileSystem) Walk(root string, walkFn filepath.WalkFunc) error {
+	f.mu.RLock()
+	defer f.mu.RUnlock()
+	paths := []string{}
+	root = filepath.Clean(root)
+	dir := root + "/"
+	if root == "/" {
+		dir = "/"
+	}
+	for path := range f.files {
+		if path == root || strings.HasPrefix(path, dir) {
+			paths = append(paths, path)
+		}
+	}
+	sort.Strings(paths)
+	var err error
+	for _, path := range paths {
+		info := f.files[path]
+		if info.failStat {
+			err = errStatFailure
+		}
+		// TODO(tav): Add support for filepath.SkipDir.
+		if err := walkFn(path, info, err); err != nil {
+			return err
+		}
+		err = nil
+	}
+	return nil
+}
+
+// WriteFile creates a file with the given data at the specified path. It will
+// implicitly create any parent directories as necessary.
+func (f *FileSystem) WriteFile(path string, data string) *FileInfo {
+	f.mu.Lock()
+	path = filepath.Clean("/" + path)
+	dir, name := filepath.Split(path)
+	info := &FileInfo{
+		data: data,
+		name: name,
+	}
+	f.files[path] = info
+	f.mu.Unlock()
+	if dir != "/" {
+		f.Mkdir(dir)
+	}
+	return info
+}
+
+// NewFileSystem returns a mockable filesystem for testing purposes.
+func NewFileSystem() *FileSystem {
+	return &FileSystem{
+		files: map[string]*FileInfo{
+			"/": &FileInfo{
+				dir: true,
+			},
+		},
+	}
+}
---
+++ pkg/mock/sys/sys_test.go
@@ -0,0 +1,110 @@
+// Public Domain (-) 2020-present, The Core Authors.
+// See the Core UNLICENSE file for details.
+
+package sys
+
+import (
+	"io"
+	"os"
+	"testing"
+)
+
+func TestFS(t *testing.T) {
+	fs := NewFileSystem()
+	path := "/path/to/test"
+	fs.WriteFile(path, "hello world")
+	info, err := fs.Stat(path)
+	if err != nil {
+		t.Fatalf("failed to stat file: %s", err)
+	}
+	if info.IsDir() {
+		t.Fatal("unexpected reporting of file as directory in file info")
+	}
+	if !info.ModTime().IsZero() {
+		t.Fatal("unexpected modtime value in file info")
+	}
+	if !info.Mode().IsRegular() {
+		t.Fatal("unexpected reporting of file mode in file info")
+	}
+	if info.Name() != "test" {
+		t.Fatalf("file info contained the wrong name: got %q, want %q", info.Name(), "test")
+	}
+	if info.Size() != 11 {
+		t.Fatalf("file info contained the wrong size: got %d, want 11", info.Size())
+	}
+	if info.Sys() == nil {
+		t.Fatal("unexpected sys value in file info")
+	}
+	f, err := fs.Open(path)
+	if err != nil {
+		t.Fatalf("failed to open file: %s", err)
+	}
+	buf := make([]byte, 11)
+	if _, err := io.ReadFull(f, buf); err != nil {
+		t.Fatalf("failed to read file: %s", err)
+	}
+	if string(buf) != "hello world" {
+		t.Fatalf("got wrong contents when reading file: got %q; want %q", string(buf), "hello world")
+	}
+	if err := f.Close(); err != nil {
+		t.Fatalf("failed to close file: %s", err)
+	}
+	if err := f.Close(); err == nil {
+		t.Fatal("successfully closed already closed file")
+	}
+	if _, err := io.ReadFull(f, buf); err == nil {
+		t.Fatal("successfully read already closed file")
+	}
+	path = "/some/other/path"
+	fs.WriteFile(path, "hello world").FailRead().FailClose()
+	f, err = fs.Open(path)
+	if err != nil {
+		t.Fatalf("failed to open file: %s", err)
+	}
+	if _, err := io.ReadFull(f, buf); err == nil {
+		t.Fatal("successfully read file marked to FailRead")
+	}
+	if err := f.Close(); err == nil {
+		t.Fatal("successfully closed file marked to FailClose")
+	}
+	info, err = fs.Stat("/some/other")
+	if err != nil {
+		t.Fatalf("failed to stat directory: %s", err)
+	}
+	if !info.IsDir() || !info.Mode().IsDir() {
+		t.Fatal("info does not report path as directory")
+	}
+	info = fs.Mkdir("/some/other")
+	if !info.IsDir() || !info.Mode().IsDir() {
+		t.Fatal("info does not report path as directory")
+	}
+	info = fs.Mkdir("/some/other/subpath")
+	if !info.IsDir() || !info.Mode().IsDir() {
+		t.Fatal("info does not report path as directory")
+	}
+	path = "/does/not/exist"
+	if _, err := fs.Lstat(path); err == nil {
+		t.Fatal("successfully did lstat on non-existent file")
+	}
+	if _, err := fs.Open(path); err == nil {
+		t.Fatal("successfully opened non-existent file")
+	}
+	if err := fs.Walk("/", func(path string, info os.FileInfo, err error) error {
+		return err
+	}); err != nil {
+		t.Fatalf("failed to walk path: %s", err)
+	}
+	path = "/broken/file"
+	fs.WriteFile(path, "some data").FailStat()
+	if _, err := fs.Open(path); err == nil {
+		t.Fatal("successfully opened path marked to FailStat")
+	}
+	if _, err := fs.Stat(path); err == nil {
+		t.Fatal("successfully did stat on path marked to FailStat")
+	}
+	if err := fs.Walk("/", func(path string, info os.FileInfo, err error) error {
+		return err
+	}); err == nil {
+		t.Fatal("successfully walked path marked to FailStat")
+	}
+}
