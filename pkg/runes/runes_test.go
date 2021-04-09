4964e49615440d82600b12f4506b2bd30009ba25
tav <tav@espra.com>
2021-04-09 05:39:56 +0100

    pkg/runes: benchmark ToBytes against the builtin functions

--- pkg/runes/runes_test.go
+++ pkg/runes/runes_test.go
@@ -8,8 +8,35 @@ import (
 	"testing"
 )
 
+var (
+	saveLen int
+)
+
+var (
+	textLarge = []rune("万難定団介問点退外落止泰上年広連。般真泉事危択際接徴外都載夜再。険芋健行掲進陥表月数的災著索暮著終見者事。即阜必得迅同面望勢題転四。治終雪都質佐更認幅関億運訃況彼。献続決芸県載木対造高細言青。索時権解情科元動立視月役地関行夕。席案市善済故刑目井疑発分質外町書天高曝図。覧取権徹明懸訴局利間両保録代降芸女全。")
+	textSmall = []rune("Hello, 世界")
+)
+
 type testrunes struct {
 	runes []rune
+}
+
+func BenchmarkToBytes(b *testing.B) {
+	l := 0
+	for i := 0; i < b.N; i++ {
+		l += len(ToBytes(textLarge))
+		l += len(ToBytes(textSmall))
+	}
+	saveLen = l
+}
+
+func BenchmarkToBytesSimple(b *testing.B) {
+	l := 0
+	for i := 0; i < b.N; i++ {
+		l += len([]byte(string(textLarge)))
+		l += len([]byte(string(textSmall)))
+	}
+	saveLen = l
 }
 
 func TestToBytes(t *testing.T) {
